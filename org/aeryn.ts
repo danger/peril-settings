781038534a58751060a5258021a71dde72dcd263
Orta Therox <orta.therox@gmail.com>
2017-12-19 09:54:29 -0500

    Refactors

--- org/aeryn.ts
+++ org/aeryn.ts
@@ -18,11 +18,12 @@ const rfc: any = isJest ? storeRFC : runRFC
 export const aeryn = rfc("When a PR is merged, check if the author is in the org", async () => {
 
   const pr = danger.github.pr
-  const authorLogin = pr.user.login
+  const username = pr.user.login
+  const api = danger.github.api
 
   const org = "danger"
   const inviteMarkdown = `
-Thanks for the PR @${authorLogin}.
+Thanks for the PR @${username}.
 
 We conform to the [Moya Community Continuity Guidelines][moya_cc], which means
 that we want to offer any contributor the ability to control their destiny.
@@ -33,13 +34,10 @@ So, we've sent you an org invite - thanks :tada:
 `
 
   try {
-    await danger.github.api.orgs.checkMembership({
-      org, 
-      username: authorLogin 
-    })
+    await api.orgs.checkMembership({ org, username })
   } catch (error) {
-    await danger.github.api.orgs.addOrgMembership({ org, username: authorLogin, role: "member" })
     // Ideally we'd write `markdown` but it looks like the scheduler isn't working as expected here?
-    await danger.github.api.issues.createComment({ ...danger.github.thisPR, body: inviteMarkdown })
+    await api.issues.createComment({ ...danger.github.thisPR, body: inviteMarkdown })
+    await api.orgs.addOrgMembership({ org, username, role: "member" })
   }
 })
