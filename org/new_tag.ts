b27c63ef0d44027379b76e53cae654b576ae80be
Orta Therox <orta.therox@gmail.com>
2017-12-31 14:00:21 -0500

    WIP support for saying thanks to closed PRs

--- org/new_tag.ts
+++ org/new_tag.ts
@@ -1,5 +1,6 @@
 import { schedule, danger, markdown } from "danger"
-import {Create} from "github-webhook-event-types"
+import { Create } from "github-webhook-event-types"
+import * as semverSort from "semver-sort"
 
 declare const peril: any // danger/danger#351
 const isJest = typeof jest !== "undefined"
@@ -15,28 +16,64 @@ const runRFC = (reason: string, closure: () => void | Promise<any>) => schedule(
 
 const rfc: any = isJest ? storeRFC : runRFC
 
+// TODO:
+// * Remove mentions on PRs from tag author
+// * 7ea07170-ee5e-11e7-827d-967e155710e3
+//
 export const newTag = rfc("Send a comment to PRs on new tags that they have been released", async () => {
   const api = danger.github.api
-  const gh = danger.github as any as Create
+  const gh = (danger.github as any) as Create
   const tag = gh.ref
+  const thisRepo = { owner: gh.repository.owner.login, repo: gh.repository.name }
 
-  await api.repos.compareCommits()
+  const allTags: Tag[] = await api.repos.getTags(thisRepo)
+  console.log(allTags)
+  const semvers: string[] = semverSort.asc(allTags.map(tag => tag.name))
 
+  const versionIndex = semvers.findIndex(version => version === tag)
+  const releaseMinusOne = semvers[versionIndex + 1]
+  console.log(`Looking between ${tag} and ${releaseMinusOne}`)
+  if (!releaseMinusOne) {
+    return
+  }
+
+  const compareData: CompareResults = await api.repos.compareCommits({ ...thisRepo, base: releaseMinusOne, head: tag })
+  console.log("compare:", compareData)
+  const commitMessages = compareData.commits.map(c => c.commit.name)
+  const prMerges = commitMessages.filter(message => message.startsWith("Merge pull request #"))
+  const numberExtractor = /Merge pull request #(\d*)/
+  const prs = prMerges
+    .map(msg => msg.match(numberExtractor) && msg.match(numberExtractor)![1])
+    .filter(pr => pr) as string[]
+  console.log("prs:", prs)
 
   const changelogURL = gh.repository.html_url + "/master/CHANGELOG.md"
-  const inviteMarkdown = `
+
+  const inviteMarkdown = (username: string) => `
   Thanks for the PR @${username}.
 
   This PR has been shipped in v${gh.ref} - [CHANGELOG][].
   
   [CHANGELOG]: ${changelogURL}
   `
-
-
-  // try {
-  //   await api.orgs.checkMembership({ org, username })
-  // } catch (error) {
-  //   markdown(inviteMarkdown)
-  //   await api.orgs.addOrgMembership({ org, username, role: "member" })
-  // }
 })
+
+interface Tag {
+  name: string
+  commit: {
+    sha: string
+    url: string
+  }
+  zipball_url: string
+  tarball_url: string
+}
+
+interface Commit {
+  commit: {
+    name: string
+  }
+}
+
+interface CompareResults {
+  commits: Commit[]
+}
