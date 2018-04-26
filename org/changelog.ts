08dfe4e66d4ce36dd1b2e6319a9227349d4a0f82
Orta Therox <orta.therox@gmail.com>
2018-04-26 16:58:04 -0400

    Move to the new default export option

--- org/changelog.ts
+++ org/changelog.ts
@@ -1,9 +1,9 @@
 import { danger, warn } from "danger"
 
-;(async () => {
+export default async () => {
   const pr = danger.github.pr
 
-  const changelogs = ["CHANGELOG.md", "changelog.md", "CHANGELOG.yml"]
+  const changelogNames = ["CHANGELOG.md", "changelog.md", "CHANGELOG.yml"]
   const isOpen = danger.github.pr.state === "open"
 
   // Get all the files in the root folder of the repo
@@ -11,22 +11,22 @@ import { danger, warn } from "danger"
   const getContentParams = { owner: pr.base.user.login, repo: pr.base.repo.name, sha: "master" }
   const rootContents: any = await danger.github.api.gitdata.getTree(getContentParams)
 
-  const hasChangelog = rootContents.data.tree.find((file: { path: string }) => changelogs.includes(file.path))
-  if (isOpen && hasChangelog) {
+  const pathForChangelog = rootContents.data.tree.find((file: { path: string }) => changelogNames.includes(file.path))
+  if (isOpen && pathForChangelog) {
     const files = [...danger.git.modified_files, ...danger.git.created_files]
 
-    // A rough heuristic for when code should havea changelog
+    // A rough heuristic for when code should have a changelog
     const hasCodeChanges = files.find(file => !file.match(/(test|spec|readme|changelog)/i))
     // Check for a changelog
-    const hasChangelogChanges = files.find(file => changelogs.includes(file))
+    const hasChangelogChanges = files.find(file => changelogNames.includes(file))
 
     // Request a CHANGELOG entry if not declared #trivial
     const isTrivial = (danger.github.pr.body + danger.github.pr.title).includes("#trivial")
     const isUser = danger.github.pr.user.type === "User"
 
     // Politely ask for their name on the entry too
-    if (hasCodeChanges && !hasChangelog && !isTrivial && !isUser) {
-      const changelogDiff = await danger.git.diffForFile("CHANGELOG.md")
+    if (hasCodeChanges && !pathForChangelog && !isTrivial && !isUser) {
+      const changelogDiff = await danger.git.diffForFile(pathForChangelog)
       const contributorName = danger.github.pr.user.login
       if (changelogDiff && changelogDiff.diff.includes(contributorName)) {
         warn("Please add your GitHub name to the changelog entry, so we can attribute you correctly.")
@@ -35,4 +35,4 @@ import { danger, warn } from "danger"
       }
     }
   }
-})()
+}
