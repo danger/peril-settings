6ae3b8ad199a7686fae5690da66ab81a931f1bfa
Orta Therox <orta.therox@gmail.com>
2018-05-25 20:41:51 -0400

    Triggers a redeploy of danger.systems when a danger project gets a new tag

---
+++ org/updateDangerSystems.ts
@@ -0,0 +1,14 @@
+import { peril } from "danger"
+import { Create } from "github-webhook-event-types"
+
+import fetch from "node-fetch"
+
+export default async (gh: Create) => {
+  const gitlabProjectID = "1620437"
+
+  await fetch(`https://gitlab.com/api/v3/projects/${gitlabProjectID}/pipeline?ref=master`, {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "PRIVATE-TOKEN": peril.env.GITLAB_DEPLOY_TOKEN,
+  })
+}
