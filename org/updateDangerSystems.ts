bea200de68a73db877d7bfca6a9b8d6d35066bc4
Orta Therox <orta.therox@gmail.com>
2018-07-08 15:15:21 +0100

    POST

--- org/updateDangerSystems.ts
+++ org/updateDangerSystems.ts
@@ -6,14 +6,12 @@ import fetch from "node-fetch"
 export default async (gh: Create) => {
   const gitlabProjectID = "1620437"
 
-  const response = await fetch(`https://gitlab.com/api/v3/projects/${gitlabProjectID}/pipeline?ref=master`, {
+  await fetch(`https://gitlab.com/api/v3/projects/${gitlabProjectID}/pipeline?ref=master`, {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
       "PRIVATE-TOKEN": peril.env.GITLAB_DEPLOY_TOKEN,
     },
   })
-
-  console.log("> ", peril.env.GITLAB_DEPLOY_TOKEN)
-  console.log(JSON.stringify(response, null, "  "))
 }
